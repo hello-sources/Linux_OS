ad2734a5bab57b16419463182c1894bd748c28c2
ltw <3245849061@qq.com>
2020-04-27 17:01:11 +0800

    CPU信息查看

--- 3.Linux综合项目/0.分布式集群监控系统/Script/1.Cpu.sh
+++ 3.Linux综合项目/0.分布式集群监控系统/Script/1.Cpu.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
-
 Time=`date +"%Y-%m-%d__%H:%M:%S"`
 LoadAvg=`cut -d " " -f 1-3 /proc/loadavg`
 CpuTemp=`cat /sys/class/thermal/thermal_zone0/temp`
-CpuTemp=`echo "scale=2;${CpuTemp}/1000" | bc`
+CpuTemp=`echo "scale=2;$CpuTemp/1000" | bc`
 
-eval $(head -n 1 /proc/stat | awk -v Sum1=0 \ 
-    '{for(i=2;i<=11;i++){Sum1=Sum1+$i} printf("Sum1=%d;Idle1=%d", Sum1, $5)}')
+eval $(head -n 1 /proc/stat | awk -v sum1=0 -v idle1=0 \
+	'{for (i=2;i<=8;i++) {sum1=sum1+$i} printf("sum1=%d;idle1=%d;",sum1,$5)}')
 
 sleep 0.5
 
-eval $(head -n 1 /proc/stat | awk -v Sum2=0 \
-    '{for(i=2;i<=11;i++){Sum2=Sum2+$i} printf("Sum2=%d;Idle2=%d", Sum2, $5)}')
+eval $(head -n 1 /proc/stat | awk -v sum2=0 -v idle2=0 \
+	'{for (i=2;i<=8;i++) {sum2=sum2+$i} printf("sum2=%d;idle2=%d;",sum2,$5)}')
 
-CupUsedPerc=`echo "scale=4;(1-(${Idle2}-${Idle1})/(${Sum2}-${Sum1})*100)" | bc`
-CupUsedPerc=`printf "%.2f" "${CpuUsedPerc}"`
+CpuUsedPerc=`echo "scale=4;(1-($idle2-$idle1)/($sum2-$sum1))*100" | bc`
+CpuUsedPerc=`printf "%.2f\n" "$CpuUsedPerc"`
 
 WarnLevel="normal"
 
-if [[ `echo "${CpuTemp} >= 70" | bc -l` == 1 ]];then
-    WarnLevel="warning"
-elif [[ `echo "${CupTemp} >= 50" | bc -l` == 1 ]]; then
-    WarnLevel="note"
+if [[ `echo $CpuTemp '>=' 70 | bc -l` -eq 1 ]]; then
+	WarnLevel="warning"
+elif [[ `echo $CpuTemp '>=' 50 | bc -l` -eq 1 ]]; then
+	WarnLevel="note"
 fi
 
-echo "${Time} ${LoadAvg} ${CupUsedPerc} ${CpuTemp}'C $WarnLevel"
-
-
-
-
-
+echo "${Time} ${LoadAvg} ${CpuUsedPerc} ${CpuTemp}°C $WarnLevel" 
