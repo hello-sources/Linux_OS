602325b98e15ec40c5adda2cc161dbf4a7c70b28
ltw <3245849061@qq.com>
2020-06-09 12:24:00 +0800

    UDP_football公共文件

--- 2.socket_project/一个小游戏/1.football_game/common/udp_epoll.c
+++ 2.socket_project/一个小游戏/1.football_game/common/udp_epoll.c
@@ -1,25 +1,24 @@
-/************************************************************
-    File Name : udp_epoll.c
-    Author: ltw
-    Mail: 3245849061@qq.com
-    Github: https://github.com/Ginakira
-    Created Time: 2020/06/04 19:07:10
-************************************************************/
+/*************************************************************************
+	> File Name: udp_epoll.c
+	> Author: ltw
+	> Mail: 3245849061@qq.com
+	> Github: https://github.com/hello-sources
+	> Created Time: Mon 08 Jun 2020 09:29:48 PM CST
+ ************************************************************************/
 
+#include "./color.h"
 #include "./common.h"
 #include "./udp_client.h"
 #include "./udp_server.h"
 #include "./head.h"
-
 extern int port;
 
-void add_event(int epollfd, int fd, int events) {
+void add_event(int epollfd, int fd, int events){
     struct epoll_event ev;
     ev.events = events;
     ev.data.fd = fd;
     epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev);
-    DBG(YELLOW "EPOLL" NONE " : After Epoll Add.\n");
-    return;
+    DBG(YELLOW"EPOLL"NONE" : After Epoll Add.\n");
 }
 
 void del_event(int epollfd, int fd, int events) {
@@ -27,22 +26,22 @@ void del_event(int epollfd, int fd, int events) {
     ev.data.fd = fd;
     ev.events = events;
     epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &ev);
-    return;
 }
 
-int udp_connect(int epollfd, struct sockaddr_in * serveraddr) {
+int udp_connect(int epollfd, struct sockaddr_in *serveraddr) {
     int sockfd;
-    if ((sockfd = socket_create_udp(8888)) < 0) {
+    if ((sockfd = socket_create_udp(port)) < 0) {
         perror("socket_udp");
         return -1;
     }
-    if ((connect(sockfd, (struct sockaddr *)serveraddr, sizeof(struct sockaddr))) < 0) {
+    DBG(GREEN"INFO"NONE" : Before connect.\n");
+    if (connect(sockfd, (struct sockaddr *)serveraddr, sizeof(struct sockaddr)) < 0) {
         perror("connect");
         return -1;
     }
-    DBG(GREEN "INFO" NONE " : After connect.\n");
-    int ret = send(sockfd, "Logged!", sizeof("Logged!"), 0);
-    DBG(RED "RET= %d\n" NONE, ret);
+    DBG(GREEN"INFO"NONE" : After connect.\n");
+    int ret = send(sockfd, "Login!", sizeof("Login!"), 0);
+    DBG(RED"RET = %d\n"NONE, ret);
     add_event(epollfd, sockfd, EPOLLIN);
     return sockfd;
 }
@@ -50,27 +49,28 @@ int udp_connect(int epollfd, struct sockaddr_in * serveraddr) {
 int udp_accept(int epollfd, int fd) {
     struct sockaddr_in client;
     int new_fd, ret;
-	struct LogRequest request;
-	struct LogResponse response;
-	bzero(&request, sizeof(request));
-	bzero(&response, sizeof(response));
-    socklen_t len = sizeof(struct sockaddr_in);
+    struct LogRequest request;
+    struct LogResponse response;
+
+    bzero(&request, sizeof(request));
+    bzero(&response, sizeof(response));
+
+    socklen_t len  = sizeof(struct sockaddr_in);
     ret = recvfrom(fd, (void *)&request, sizeof(request), 0, (struct sockaddr *)&client, &len);
     if (ret != sizeof(request)) {
-		response.type = 1;
-		strcpy(response.msg, "Login failed.");
-		sendto(fd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&client, len);
+        response.type = 1;
+        strcpy(response.msg, "Login failed.");
+        sendto(fd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&client, len);
         return -1;
     }
-	response.type = 0;
+    response.type = 0;
     strcpy(response.msg, "Login success. Enjoy yourself.");
-	sendto(fd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&client, len);
+    sendto(fd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&client, len);
 
-	if (request.team) 
-		DBG(GREEN "INFO" NONE " : "BLUE" %s on %s : %d login! (%s)\n"NONE, request.name, inet_ntoa(client.sin_addr), ntohs(client.sin_port), response.msg);
-	else	
-		DBG(GREEN "INFO" NONE " : "RED" %s on %s : %d login! (%s)\n"NONE, request.name, inet_ntoa(client.sin_addr), ntohs(client.sin_port), response.msg);
-    //DBG(PINK "RECV" NONE ": %s\n", msg);
+    if (request.team)
+        DBG(GREEN"INFO"NONE" : "BLUE" %s on %s:%d login! (%s)\n"NONE, request.name, inet_ntoa(client.sin_addr), ntohs(client.sin_port), request.msg);
+    else
+        DBG(GREEN"INFO"NONE" : "RED" %s on %s:%d login! (%s)\n"NONE, request.name, inet_ntoa(client.sin_addr), ntohs(client.sin_port), request.msg);
 
     new_fd = udp_connect(epollfd, &client);
     return new_fd;
--- 2.socket_project/一个小游戏/1.football_game/common/udp_epoll.h
+++ 2.socket_project/一个小游戏/1.football_game/common/udp_epoll.h
@@ -1,10 +1,11 @@
-/************************************************************
-    File Name : udp_epoll.h
-    Author: ltw
-    Mail: 3245849061@qq.com
-    Github: https://github.com/Ginakira
-    Created Time: 2020/06/04 19:24:03
-************************************************************/
+/*************************************************************************
+	> File Name: udp_epoll.h
+	> Author: ltw
+	> Mail: 3245849061@qq.com
+	> Github: https://github.com/hello-sources
+	> Created Time: Mon 08 Jun 2020 09:30:23 PM CST
+ ************************************************************************/
+
 
 #ifndef _UDP_EPOLL_H
 #define _UDP_EPOLL_H
